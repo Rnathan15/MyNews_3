97d5bcadced4a309824cd9abc69a1b91ffa07517
Raphi Nathan <raphinathan@Raphis-MacBook-Pro-2.local>
2023-02-17 01:54:01 +0200

    Initial Commit 2

---
+++ backend.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const API_KEY = 'your_api_key_here';
+
+app.post('/summarize', async (req, res) => {
+  const { url } = req.body;
+  const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+    prompt: `Summarize the article at ${url}`,
+    max_tokens: 100,
+    n: 1,
+    stop: ['\n']
+  }, {
+    headers: {
+      'Authorization': `Bearer ${API_KEY}`,
+      'Content-Type': 'application/json'
+    }
+  });
+  const summary = response.data.choices[0].text.trim();
+  res.send(summary);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
